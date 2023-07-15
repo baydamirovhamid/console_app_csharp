5d32866b172a8b0c6b2808dbe3aebfde40cfa68e
baydamirov <138532596+baydamirov@users.noreply.github.com>
2023-07-16 00:36:21 +0400

    basiccreature

---
+++ basiccreature.cs
@@ -0,0 +1,19 @@
+using System;
+namespace backend01
+{
+
+    class creature
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("  .-."); ;
+            Console.WriteLine(" (o o) ");
+            Console.WriteLine(" | O |");
+            Console.WriteLine(" |   |");
+            Console.WriteLine(" '~~~'");
+             
+
+            Console.ReadKey();
+        }
+    }
+}
