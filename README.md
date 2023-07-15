48e6eba6847d86309d73031647f6b08275a060ba
baydamirov <138532596+baydamirov@users.noreply.github.com>
2023-07-15 09:58:07 +0400

    simple creature adding

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # console_app.csharp
+
+1. Simple creature
+![Simple Creature](simple_creature.png)
---
+++ simple_creature.cs
@@ -0,0 +1,28 @@
+using System;
+namespace holmescreature
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("c_      __");
+            Console.WriteLine("   \\\\__|__|__");
+            Console.WriteLine("     \\(o_o)");
+            Console.WriteLine("      > ~ >");
+            Console.WriteLine("     /   / \\\\");
+            Console.WriteLine("    /   /   \\\\");
+            Console.WriteLine("    \\   )    c/ ");
+            Console.WriteLine("   /   /");
+            Console.WriteLine("  /   /|");
+            Console.WriteLine("  (  ( \\ ");
+            Console.WriteLine("  |  |  \\ ");
+            Console.WriteLine("  | / \\  ) ");
+            Console.WriteLine("  | |  ) | ");
+            Console.WriteLine(" /  )(_/ ");
+            Console.WriteLine("(_ / ");
+
+            Console.ReadKey();
+        }
+    }
+}
